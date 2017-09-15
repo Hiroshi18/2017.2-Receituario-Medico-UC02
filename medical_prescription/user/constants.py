c71c8da14b74fe7d37cc07a3d469d8d8a69532c5
Unknown <lucas.hh@hotmail.com>
2017-09-15 12:50:27 -0300

    create

---
+++ medical_prescription/user/constants.py
@@ -0,0 +1,22 @@
+"""This is a constant file."""
+# NAME FIELDS.
+NAME = 'Name'
+NAME_FIELD_LENGTH = 55
+NAME_MIN_LENGTH = 5
+NAME_MAX_LENGHT = 50
+
+# NAME VALIDATION MESSAGES
+NAME_SIZE = 'Your name exceeds 50 characteres'
+NAME_CHARACTERS = 'Your name can\'t have special characters'
+NAME_FORMAT = 'Your name must have just letters'
+USERNAME_SIZE = 'Name must be between 5 and 50 characteres'
+
+# PASSWORDS FIELDS.
+PASSWORD = 'Password'
+PASSWORD_FIELD_LENGTH = 30
+PASSWORD_MIN_LENGTH = 8
+PASSWORD_MAX_LENGTH = 12
+
+# PASSWORD VALIDATION MESSAGES.
+PASSWORD_SIZE = 'Password must be between 8 and 12 characters'
+PASSWORD_NOT_EQUAL = 'Passwords do not match.'
